f1ddf395150f71cb6cd8a42e5daab08709a54ee0
Róbert Kovács <robert.kovacs.centacc@gmail.com>
2019-01-16 19:51:51 +0100

    angular-tour-of-heroes - in-memory-data

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Hero } from './heroes/hero';
-import { HEROES } from './heroes/mock-heroes';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { Hero } from './heroes/hero';
 import { MessageService } from './message.service';
 
 @Injectable({
@@ -9,15 +11,49 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl: string = 'api/heroes';
+
+  constructor(
+    private http: HttpClient, 
+    private messageService: MessageService
+  ) { }
 
   getHeroes(): Observable<Hero[]> {
   	this.messageService.addMessage('HeroService: fetched heroes');
-  	return of(HEROES);
+  	return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
 
-  getHero(id: number) {
-  	this.messageService.addMessage(`HeroService: fetched hero id=${id}`);
-  	return of(HEROES.find(hero => hero.id === id));
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+  	return this.http.get<Hero>(url)
+      .pipe(
+        tap(_ => this.log(`ffetched hero id=${id}`)),
+        catchError(this.handleError<Hero>('getHero id=${id}'))
+      );
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+     return this.http.put(this.heroesUrl, hero, httpOptions)
+       .pipe(
+         tap(_ => this.log(`updated hero id=${hero.id}`)),
+         catchError(this.handleError<any>('updateHero'))
+       );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
+
+  private log(message: string) {
+    this.messageService.addMessage(`HeroService: ${message}`);
   }
 }
--- angular-tour-of-heroes/src/app/heroes/mock-heroes.ts
+++
@@ -1,6 +0,0 @@
-import { Hero } from './hero';
-
-export const HEROES: Hero[] = [
-	{id: 1, name: 'RubberMan'},
-	{id: 2, name: 'Dynama'},
-];
