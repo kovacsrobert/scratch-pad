2d29f17f4371a58be5113bfbb6d09bb6e4bedab6
Robert Kovacs <robert.kovacs.centacc@gmail.com>
2019-03-05 14:31:25 +0100

    added web-logging-demo generated project

---
+++ web-logging-demo/src/main/java/com/example/webloggingdemo/WebLoggingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.webloggingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLoggingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebLoggingDemoApplication.class, args);
+	}
+
+}
