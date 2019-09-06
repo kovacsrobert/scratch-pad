bda06c38af1b8dbd3bf829dc3007abdbbd7c69fa
Robert Kovacs <robert.kovacs.centacc@gmail.com>
2019-09-06 21:42:43 +0200

    remove extracted cluster demo

--- cluster-demo/cluster-web/src/main/java/hello/web/config/Profiles.java
+++
@@ -1,9 +0,0 @@
-package hello.web.config;
-
-public interface Profiles {
-
-	String IGNORE_CACHING = "ignoreCaching";
-
-	String FAKE_DAO = "fakeDao";
-
-}
--- cluster-demo/cluster-web/src/main/java/hello/web/provider/HelloProvider.java
+++
@@ -1,5 +0,0 @@
-package hello.web.provider;
-
-public interface HelloProvider {
-	String welcome(String name);
-}
