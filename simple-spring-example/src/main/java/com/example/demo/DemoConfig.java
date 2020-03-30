145cc220e1596ce71b8727f4f201e3531020f3ae
Robert Kovacs <robert.kovacs.centacc@gmail.com>
2020-03-30 14:59:05 +0200

    message source test #2

--- simple-spring-example/src/main/java/com/example/demo/DemoConfig.java
+++ simple-spring-example/src/main/java/com/example/demo/DemoConfig.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 
-import org.springframework.context.MessageSource;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
@@ -9,9 +9,19 @@ import org.springframework.context.support.ReloadableResourceBundleMessageSource
 public class DemoConfig {
 
 	@Bean
-	public MessageSource messageSource() {
+	public ReloadableResourceBundleMessageSource messageSource() {
 		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
 		messageSource.setBasename("classpath:messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+
+	@Bean
+	@Qualifier("alternativeMessageSource")
+	public ReloadableResourceBundleMessageSource alternativeMessageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setParentMessageSource(messageSource());
+		messageSource.setBasename("classpath:alternatives");
 		messageSource.setDefaultEncoding("UTF-8");
 		return messageSource;
 	}
--- simple-spring-example/src/main/java/com/example/demo/DemoController.java
+++ simple-spring-example/src/main/java/com/example/demo/DemoController.java
@@ -1,25 +1,51 @@
 package com.example.demo;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Locale;
 
-import org.springframework.context.MessageSource;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
-	private final MessageSource messageSource;
+	private final ReloadableResourceBundleMessageSource alternativeMessageSource;
 
-	public DemoController(MessageSource messageSource) {
-		this.messageSource = messageSource;
+	public DemoController(
+			@Qualifier("alternativeMessageSource") ReloadableResourceBundleMessageSource alternativeMessageSource) {
+		this.alternativeMessageSource = alternativeMessageSource;
 	}
 
 	@RequestMapping("/")
 	public String home() {
-		String englishMessage = messageSource.getMessage("demo.app.test.1", null, Locale.ENGLISH);
-		String germanMessage = messageSource.getMessage("demo.app.test.1", null, Locale.GERMAN);
+		String englishMessage1 = alternativeMessageSource.getMessage("demo.app.test.1", null, Locale.ENGLISH);
+		String germanMessage1 = alternativeMessageSource.getMessage("demo.app.test.1", null, Locale.GERMAN);
+		String englishMessage2 = alternativeMessageSource.getMessage("demo.app.test.2", null, Locale.ENGLISH);
+		String germanMessage2 = alternativeMessageSource.getMessage("demo.app.test.2", null, Locale.GERMAN);
 
-		return "Hello Docker World<br />" + englishMessage + "<br />" + germanMessage;
+		return "Hello Docker World<br />" + englishMessage1 + "<br />" + germanMessage1 + "<br />" + englishMessage2 + "<br />" + germanMessage2;
+	}
+
+	@RequestMapping("/reload")
+	public String reload() throws IOException {
+		ClassPathResource englishResource = findResource("alternatives.properties");
+		IOUtils.write("demo.app.test.1=reloaded test value 1", new FileOutputStream(englishResource.getFile()), UTF_8);
+
+		ClassPathResource germanResource = findResource("alternatives_de.properties");
+		IOUtils.write("demo.app.test.1=reloaded german test value 1", new FileOutputStream(germanResource.getFile()), UTF_8);
+
+		alternativeMessageSource.clearCache();
+		return "Reloaded";
+	}
+
+	protected ClassPathResource findResource(String configurationFileName) {
+		return new ClassPathResource(configurationFileName);
 	}
 }
